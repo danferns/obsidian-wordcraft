37bb3bf7986076c24bb7d68c21c8c67f9ca97580
danferns <dannywritescode@gmail.com>
2025-03-05 17:17:59 +0530

    initial WordCraft implementation

---
+++ WordCraftView.svelte
@@ -0,0 +1,139 @@
+<script lang="ts">
+	interface Props {
+		topics: string;
+		search: string;
+		mode: number;
+	}
+
+	interface Word {
+		word: string;
+		score: number;
+		tags: string[];
+	}
+
+	let { topics = "", search = "", mode = 0 }: Props = $props();
+	let words: Word[] = $state([]);
+
+	// Reference: https://www.datamuse.com/api/
+	const BASE_URL = "https://api.datamuse.com/words";
+	const modes = [
+		{
+			name: "Means like...",
+			params: "ml=",
+		},
+		{
+			name: "Rhymes with...",
+			params: "rel_rhy=",
+		},
+		{
+			name: "Nouns to describe...",
+			params: "rel_jja=",
+		},
+		{
+			name: "Adjectives that describe...",
+			params: "rel_jjb=",
+		},
+	];
+
+	let globalParams = "max=500&topics=";
+
+	async function fetchWords() {
+		if (!search) return;
+		let url = `${BASE_URL}?${globalParams}${topics}&${modes[mode].params}${search}`;
+		console.log(url);
+		let response = await fetch(url);
+		let result: Word[] = await response.json();
+		words = result;
+	}
+
+	let timer: NodeJS.Timeout;
+
+	const debounce = () => {
+		console.log("Debouncing...");
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			fetchWords();
+		}, 200);
+	};
+
+	fetchWords();
+</script>
+
+<div class="wordcraft">
+	<input
+		bind:value={topics}
+		type="text"
+		placeholder="Topics (Optional)"
+		oninput={debounce}
+	/>
+	<input
+		bind:value={search}
+		type="text"
+		placeholder="Search for words..."
+		oninput={debounce}
+	/>
+
+	<select bind:value={mode} class="mode dropdown" onclick={fetchWords}>
+		{#each modes as mode, i}
+			<option value={i}>{mode.name}</option>
+		{/each}
+	</select>
+
+	<div class="list">
+		{#each words as word}
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<div
+				onclick={() => {
+					search = word.word;
+					// alternate between noun / adjective search
+					if (mode === 2) {
+						mode = 3;
+					} else if (mode === 3) {
+						mode = 2;
+					}
+					fetchWords();
+				}}
+			>
+				{word.word}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.wordcraft {
+		width: 100%;
+		height: 100%;
+		max-width: 400px;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		display: flex;
+		flex-direction: column;
+		padding: 8px;
+	}
+	.wordcraft > * {
+		width: 100%;
+		margin: 4px 0;
+	}
+
+	.list {
+		height: 100%;
+		overflow-y: scroll;
+		text-align: center;
+	}
+
+	.list > div {
+		padding: 8px 12px;
+		text-align: center;
+		border-radius: var(--radius-s);
+
+		display: inline-block;
+	}
+
+	.list > div:hover {
+		color: var(--text-normal);
+		background-color: var(--background-modifier-hover);
+	}
+</style>
--- main.ts
+++ main.ts
@@ -1,134 +1,74 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, ItemView, WorkspaceLeaf } from "obsidian";
+import WordCraftComponent from "./WordCraftView.svelte";
+import { mount, unmount } from "svelte";
 
-// Remember to rename these classes and interfaces!
+export const VIEW_TYPE_WORDCRAFT = "wordcraft-view";
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class WordCraft extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerView(
+			VIEW_TYPE_WORDCRAFT,
+			(leaf) => new WordCraftView(leaf)
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("book-open-text", "Open WordCraft", () => {
+			this.activateView();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-view",
+			name: "Open view",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				this.activateView();
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async activateView() {
+		const { workspace } = this.app;
 
-	}
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_WORDCRAFT);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+		if (leaves.length > 0) {
+			leaf = leaves[0];
+		} else {
+			leaf = workspace.getRightLeaf(false);
+			await leaf.setViewState({
+				type: VIEW_TYPE_WORDCRAFT,
+				active: true,
+			});
+		}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+		workspace.revealLeaf(leaf);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export class WordCraftView extends ItemView {
+	component: ReturnType<typeof WordCraftComponent> | undefined;
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+		this.icon = "book-open-text";
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getViewType() {
+		return VIEW_TYPE_WORDCRAFT;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	getDisplayText() {
+		return "WordCraft";
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async onOpen() {
+		this.component = mount(WordCraftComponent, {
+			target: this.contentEl,
+			props: { search: "", topics: "", mode: 0 },
+		});
+	}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	async onClose() {
+		if (this.component) {
+			unmount(this.component);
+		}
 	}
 }
